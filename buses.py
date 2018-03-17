74219b64d4528b77ab2ae2e9968f399ef063ef6b
tobyjamez <tobyswjames@gmail.com>
2018-03-17 12:17:19 +0000

    Buses, routes, batteries determined.

--- buses.py
+++ buses.py
@@ -1,11 +1,15 @@
+#Definitions of classes for buses, battery and route information.
+#Not to be executed, rather import
+
+if __name__ == '__main__':
+	print("Why are you doing this?")
+
 #Constants
 maintanance = 0.3 #EUR/km
 
 charging_energy = 0.1 #EUR/kW
 
 driver = 40 #EUR/h
-
-chargers = {'30':30000, '250':155000, '450':260000} #kW : EUR
 
 passenger_weight = 73 #kg
 
@@ -30,23 +34,24 @@ class C:
 
 
 #Buses
-class LE:
-	def __init__(self, battery):
+class bus:
+	def __init__(self, battery, route):
 		self.battery = battery
+		self.route = route
+
+class LE(bus):
 	price = 350000 #EUR
 	length = 9950 #mm
 	max_weight = 14870 #kg
 	weight = 7930 #kg
-class LF:
-	def __init__(self, battery):
-		self.battery = battery
+
+class LF(bus):
 	price = 390000 #EUR
 	length = 12000 #mm
 	max_weight = 19500 #kg
 	weight = 10645 #kg
-class LFA:
-	def __init__(self, battery):
-		self.battery = battery
+
+class LFA(bus):
 	price = 570000 #EUR
 	length = 18750 #mm
 	max_weight  = 29000 #kg
@@ -70,3 +75,40 @@ class HP:
 	max_discharge_I = 500 #A
 	charge_type = 'fast'
 	cycles = 6500
+
+#Routes
+#Setup in JSON-style dictionary array format
+		#km	  #min			  #min	                   #min                    #min                    #min       #m         #s
+def makeRoute(distance, duration, t1_passengers, t1_freq, t2_passengers, t2_freq, t3_passengers, t3_freq, t4_passengers, t4_freq, stop_freq, stop_duration):
+	routeDict   =  {'distance':distance,
+			'duration':duration,
+			't1':
+				{'passengers':t1_passengers,
+				 'frequency': t1_freq},
+			't2':
+				{'passengers':t2_passengers,
+				 'frequency': t2_freq},
+			't3':
+				{'passengers':t3_passengers,
+				 'frequency': t3_freq},
+			't4':
+				{'passengers':t4_passengers,
+				 'frequency': t4_freq},
+			'stop_freq':stop_freq,
+			'stop_duration':stop_duration}
+
+	return routeDict
+
+
+R1 = makeRoute(20, 60, 700, 10, 700, 10, 700, 10, 700, 10, 10000, 8*60)
+R2 = makeRoute(10, 40, 2000, 10, 300, 20, 2000, 10, 300, 20, 300, 10)
+R3 = makeRoute(15, 55, 2000, 10, 300, 20, 2000, 10, 300, 20, 500, 10)
+R4 = makeRoute(7, 20, 300, 20, 200, 30, 300, 20, 200, 30, 700, 10)
+R5 = makeRoute(6, 20, 2000, 10, 300, 10, 2000, 10, 300, 10, 300, 10)
+R6 = makeRoute(20, 60, 1500, 10, 200, 20, 1500, 10, 200, 20, 1000, 20)
+R7 = makeRoute(10, 40, 3300, 10, 1000, 10, 3300, 10, 1000, 10, 500, 10)
+R8 = makeRoute(25, 70, 1600, 20, 800, 20, 1600, 20, 800, 20, 1000, 20)
+R9 = makeRoute(35, 60, 600, 30, 200, 30, 600, 30, 200, 30, 2000, 20)
+R10= makeRoute(30, 60, 1000, 30, 600, 30, 1000, 30, 600, 30, 3000, 20)
+R11= makeRoute(40, 70, 400, 30, 100, 60, 400, 30, 100, 60, 3000, 20)
+R12= makeRoute(35, 60, 1000, 30, 200, 60, 1000, 60, 200, 30, 2000, 20)
