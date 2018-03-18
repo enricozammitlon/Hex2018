97c5a8a453a708f4f117ad71ba62a8c71575a44b
Enrico <enrico.zammitl@gmail.com>
2018-03-18 05:01:53 +0100

    Working

--- GUI.py
+++ GUI.py
@@ -14,7 +14,7 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import random
 from time import sleep
-
+import json
 APP_NAME="Down To Bus-iness"
 
 try:
@@ -33,11 +33,71 @@ except AttributeError:
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
+
+        self.AllData=[
+            {
+                "route":1,
+                "noOfBusses":2,
+                "busses":[
+                    {
+                        "ID" :1,
+                        "Type":"LE",
+                        "Velocity":0.1
+                    },
+                    {
+                        "ID":2,
+                        "Type":"LF",
+                        "Velocity":0.1
+                    }
+                ]
+            },
+            {
+                "route":2,
+                "noOfBusses":2,
+                "busses":[
+                    {
+                        "ID":1,
+                        "Type":"LE",
+                        "Velocity":0.1
+                    },
+                    {
+                        "ID":2,
+                        "Type":"LF",
+                        "Velocity":0.1
+                    }
+                ]
+
+            },
+            {
+                "route":3,
+                "noOfBusses":3,
+                "busses":[
+                    {
+                        "ID":1,
+                        "Type":"LE",
+                        "Velocity":0.1
+                    },
+                    {
+                        "ID":2,
+                        "Type":"LF",
+                        "Velocity":0.1
+                    },
+                    {
+                        "ID":3,
+                        "Type":"LF",
+                        "Velocity":0.1
+                    }
+                ]
+
+            }
+            #,{},{},{},{},{},{},{},{},{},{},{}
+        ]
+
+
         MainWindow.setObjectName(_fromUtf8(APP_NAME))
         MainWindow.resize(800, 600)
-        self.colorCodes={"Type1":'blue',"Type2":'red',"Type3":'green'}
-        self.counter=0
-        self.lines=[]
+        self.colorCodes={"LE":"blue","LF":"red","LFA":"green"}
+        self.drawings=[]
         self.pres=[]
         self.centralWidget = QtGui.QWidget(MainWindow)
         self.centralWidget.setObjectName(_fromUtf8("centralWidget"))
@@ -70,11 +130,10 @@ class Ui_MainWindow(object):
         # set the layout
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self.canvas)
-        while(self.counter<=11):
-            self.plot()
+
+        self.plot()
+
         self.widget.setLayout(layout)
-        self.steps=[random.uniform(0.09,0.000001) for i in self.lines]
-        self.outbound=[1 for i in self.steps]
 
         self.playButton = QtGui.QPushButton(self.centralWidget)
         self.playButton.setGeometry(QtCore.QRect(260, 500, 83, 28))
@@ -95,16 +154,14 @@ class Ui_MainWindow(object):
         self.scrollAreaWidgetContents.setObjectName(_fromUtf8("scrollAreaWidgetContents"))
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
 
-        self.logOutput=[]
-        for i in range(self.counter):
-            self.logOutput.append(QtGui.QTextEdit(self.scrollAreaWidgetContents))
-            self.logOutput[i].setGeometry(QtCore.QRect(10, 20+35*i, 181, 31))
-            self.logOutput[i].append("%d"%i)
-            self.logOutput[i].setReadOnly(True)
-            self.logOutput[i].setLineWrapMode(QtGui.QTextEdit.NoWrap)
-            font = self.logOutput[i].font()
-            font.setFamily("Courier")
-            font.setPointSize(10)
+        self.logOutput=QtGui.QTextEdit(self.scrollAreaWidgetContents)
+        self.logOutput.setGeometry(QtCore.QRect(10, 20, 181, 51))
+        self.logOutput.setReadOnly(True)
+        self.logOutput.setLineWrapMode(QtGui.QTextEdit.NoWrap)
+        font = self.logOutput.font()
+        font.setFamily("Courier")
+        font.setPointSize(10)
+
         self.scrollArea.setGeometry(QtCore.QRect(520, 60, 211, 431))
         MainWindow.setCentralWidget(self.centralWidget)
         self.menuBar = QtGui.QMenuBar(MainWindow)
@@ -130,54 +187,76 @@ class Ui_MainWindow(object):
 
     def anim(self):
         while(self.playpause!=2):
-            for route in range(len(self.lines)):
-                for bus in range(len(route)):
-                    step=self.steps[bus]
-                    pre=self.pres[bus]
+            for route in range(len(self.drawings)):
+                for bus in range(len(self.AllData[route]['busses'])):
+                    step=self.steps[route][bus]
+                    pos=self.currentPos[route][bus]
                     if(self.playpause==2):
                         return
-                    if(self.outbound[bus]==1):
-                        pre+=step
+                    if(self.outbound[route][bus]==1):
+                        pos+=step
                     else:
-                        pre-=step
-                    if(pre>8):
-                        self.outbound[bus]=0
-                        pre=8
-                    if(pre<0):
-                        self.outbound[bus]=1
-                        pre=0
-                    self.logOutput[bus].setText("Bus %d is %2.2fkm away"%(bus,pre))
-                    self.pres[bus]=pre
-                    self.lines[bus].set_data([pre], [1])
+                        pos-=step
+                    if(pos>8):
+                        self.outbound[route][bus]=0
+                        pos=8
+                    if(pos<0):
+                        self.outbound[route][bus]=1
+                        pos=0
+                    #self.logOutput[bus].setText("Bus %d is %2.2fkm away"%(bus,pos))
+                    self.currentPos[route][bus]=pos
+                    self.drawings[route][bus].set_data([pos], [1])
                     self.canvas.draw()
                     QtCore.QCoreApplication.processEvents()
 
     def plot(self):
-        self.counter+=1
         ''' plot some random stuff '''
         # random data
-        data = [1 for i in range(9)]
-
-        # create an axis
-        self.ax = self.fig.add_subplot(12,1,self.counter)
-
-        # discards the old graph
-        self.ax.clear()
-        plt.axis('off')
-        # plot dataline
-        self.ax.plot(data, 'g')
-        self.pres.append(0)
-        for currentBus in range(self.lines):
-            self.lines[currentBus].append(plt.plot([0], [1], marker='o', markersize=6, color="red")[0])
+        ones = [1 for i in range(9)]
+        self.steps=[]
+        self.currentPos=[]
+        self.outbound=[]
+        self.totalAx=[]
+        for route in self.AllData:
+            self.ax = self.fig.add_subplot(12,1,route['route'])
+            # create an axis
+            self.drawings.append([])
+            self.steps.append([])
+            self.currentPos.append([])
+            self.outbound.append([])
+            # discards the old graph
+            self.ax.clear()
+            plt.axis('off')
+            # plot dataline
+            self.ax.plot(ones, 'g')
+            self.pres.append([0])
+            for currentBus in route['busses']:
+                self.steps[route['route']-1].append(random.uniform(0.09,0.000001))
+                self.currentPos[route['route']-1].append(0)
+                self.outbound[route['route']-1].append(1)
+                self.drawings[route['route']-1].append(plt.plot([0], [1], marker='o', markersize=6, color=self.colorCodes[currentBus["Type"]],gid="%s&%s"%(route['route'],currentBus['ID']))[0])
+            self.fig.canvas.mpl_connect('motion_notify_event', self.on_plot_hover)
+            self.totalAx.append(self.ax)
         # refresh canvas
+
         self.canvas.draw()
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate(APP_NAME, APP_NAME, None))
-        self.label.setText(_translate(APP_NAME, "Bus Status", None))
-        self.label_2.setText(_translate(APP_NAME, "MAP", None))
+        self.label.setText(_translate(APP_NAME, "Bus Information", None))
+        self.label_2.setText(_translate(APP_NAME, "Visualise", None))
         self.playButton.setText(_translate(APP_NAME, "Play", None))
         self.pauseButton.setText(_translate(APP_NAME, "Pause", None))
+
+    def on_plot_hover(self,event):
+        for ax in self.totalAx:
+            for curve in ax.get_lines():
+                if curve.contains(event)[0]:
+                    gid="%s" % curve.get_gid()
+                    if(gid!="None"):
+                        a=gid.split("&")
+                        INFO="Route Number: %s\nBus Number: %s "%(a[0],a[1])
+                        self.logOutput.setText("%s" % INFO)
 
 if __name__ == "__main__":
     import sys
